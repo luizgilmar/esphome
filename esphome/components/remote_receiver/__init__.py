5adcb46653c8f395f134e94245b600b71da8f736
luizgilmar <103142534+luizgilmar@users.noreply.github.com>
2023-04-26 19:49:21 -0300

    Update __init__.py

--- esphome/components/remote_receiver/__init__.py
+++ esphome/components/remote_receiver/__init__.py
@@ -57,7 +57,6 @@ async def to_code(config):
     for dumper in dumpers:
         cg.add(var.register_dumper(dumper))
 
-    trigger = []
     triggers = await remote_base.build_triggers(config)
     # if triggers is not None:
         for trigger in triggers:

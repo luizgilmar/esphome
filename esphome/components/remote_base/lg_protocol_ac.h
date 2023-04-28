49d123f90f37d745dbb3d2e59d3dfeba8b93915f
luizgilmar <103142534+luizgilmar@users.noreply.github.com>
2023-04-28 18:36:31 -0300

    Update lg_protocol_ac.h

--- esphome/components/remote_base/lg_protocol_ac.h
+++ esphome/components/remote_base/lg_protocol_ac.h
@@ -6,32 +6,32 @@
 namespace esphome {
 namespace remote_base {
 
-struct LGacData {
+struct LGACData {
   uint32_t data;
   uint8_t nbits;
 
-  bool operator==(const LGacData &rhs) const { return data == rhs.data && nbits == rhs.nbits; }
+  bool operator==(const LGACData &rhs) const { return data == rhs.data && nbits == rhs.nbits; }
 };
 
-class LGacProtocol : public RemoteProtocol<LGacData> {
+class LGACProtocol : public RemoteProtocol<LGACData> {
  public:
-  void encode(RemoteTransmitData *dst, const LGacData &data) override;
-  optional<LGacData> decode(RemoteReceiveData src) override;
-  void dump(const LGacData &data) override;
+  void encode(RemoteTransmitData *dst, const LGACData &data) override;
+  optional<LGACData> decode(RemoteReceiveData src) override;
+  void dump(const LGACData &data) override;
 };
 
 DECLARE_REMOTE_PROTOCOL(LGAC)
 
-template<typename... Ts> class LGacAction : public RemoteTransmitterActionBase<Ts...> {
+template<typename... Ts> class LGACAction : public RemoteTransmitterActionBase<Ts...> {
  public:
   TEMPLATABLE_VALUE(uint32_t, data)
   TEMPLATABLE_VALUE(uint8_t, nbits)
 
   void encode(RemoteTransmitData *dst, Ts... x) override {
-    LGacData data{};
+    LGACData data{};
     data.data = this->data_.value(x...);
     data.nbits = this->nbits_.value(x...);
-    LGacProtocol().encode(dst, data);
+    LGACProtocol().encode(dst, data);
   }
 };
 

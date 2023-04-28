0161b61d16e652e05c7a61c84b2ec6e264d06168
luizgilmar <103142534+luizgilmar@users.noreply.github.com>
2023-04-28 16:54:42 -0300

    Update __init__.py

--- esphome/components/remote_receiver_ac/__init__.py
+++ esphome/components/remote_receiver_ac/__init__.py
@@ -15,7 +15,7 @@ from esphome.const import (
 from esphome.core import CORE, TimePeriod
 
 AUTO_LOAD = ["remote_base"]
-remote_receiver_ns = cg.esphome_ns.namespace("remote_receiver")
+remote_receiver_ns = cg.esphome_ns.namespace("remote_receiver_ac")
 RemoteReceiverACComponent = remote_receiver_ns.class_(
     "RemoteReceiverComponent", remote_base.RemoteReceiverBase, cg.Component
 )
